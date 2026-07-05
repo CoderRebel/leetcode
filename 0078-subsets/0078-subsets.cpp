df6e15a0027869bc854caae6865f6251b0af0f49
CoderRebel <rajeevmenaria18@gmail.com>
2026-07-05 14:00:31 +0530

    Time: 2 ms (38.02%), Space: 9.9 MB (82.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int subset = 1<<n;
-        vector<vector<int>> ans = {{}};
-        for(int num = 1 ; num <= subset-1; num++){
-            vector<int> list = {};
-            for(int i = 0; i <= n-1 ; i++){
-                if(num&(1<<i)){
-                list.push_back(nums[i]);
-                }}
-            ans.push_back(list);
+        int total_subsets = 1 << n; // 2^n
+        vector<vector<int>> ans;
+        
+        // Loop from 0 to 2^n - 1 covers all subsets, including the empty set (num = 0)
+        for (int num = 0; num < total_subsets; num++) {
+            vector<int> current_subset;
+            for (int i = 0; i < n; i++) {
+                if (num & (1 << i)) {
+                    current_subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(current_subset);
         }
-
-return ans;
+        
+        return ans;
     }
 };
