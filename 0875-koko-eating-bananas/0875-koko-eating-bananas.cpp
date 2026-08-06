9884f149618cea2d802c72133456a6e39ea12eba
CoderRebel <rajeevmenaria18@gmail.com>
2026-08-06 20:11:38 +0530

    Time: 16 ms (13.24%), Space: 22.9 MB (85.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int ans;
-        int low = 1;
-        int high = INT_MIN;
-        for(int i = 0 ; i < piles.size() ; i++){
-            if(piles[i] > high) high = piles[i];
+        int low = 1 ;
+        int  high = INT_MIN;
+        int ans = -1;
+        for(int i = 0 ; i < piles.size(); i++){
+            if(high < piles[i]) high = piles[i];
         }
-        //now we have low and high 
+
         while(low <= high){
-            int mid = low + (high - low)/2;
-            //can koko finish before time at speed mid
+            int mid = low + (high-low)/2;
             long long time = 0;
             for(int i = 0 ; i < piles.size(); i++){
-                time += (piles[i]+mid-1)/mid;
+                time += (double)(piles[i] + mid -1)/mid;
             }
             if(time <= h){
                 ans = mid;
                 high = mid - 1;
             }
-            //if not
             else{
                 low = mid + 1;
             }
