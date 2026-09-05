048a483ad3dd05d1d515f34b5bb44ddee3315e66
Rajeev Menaria <rajeevmenaria18@gmail.com>
2026-09-05 17:02:16 +0530

    Time: 60 ms (51.83%), Space: 151.2 MB (63.94%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,40 @@
 class MinStack {
 public:
-    stack <pair<int,int>> st;
-
+    stack<long long> st;
     MinStack() {
-
+        
     }
-    
+    long long mini = LLONG_MAX;
     void push(int value) {
         if(st.empty()){
-            st.push({value, value});
-        }    
+            st.push(value);
+            mini = value;
+        }
         else{
-            st.push({value , min(value , st.top().second)});
+            if(mini < value) st.push(value);
+            else{
+                st.push(1LL * 2*value - mini);
+                mini = value;
+            }
         }
     }
     
     void pop() {
+        if(st.empty()) return;
+        long long x = st.top();
         st.pop();
+        if(x < mini) mini = 1LL * 2*mini - x;   
     }
     
     int top() {
-        return st.top().first;
+        if(st.empty()) return -1;
+        long long x = st.top();
+        if(mini < x) return x;
+        return mini;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
